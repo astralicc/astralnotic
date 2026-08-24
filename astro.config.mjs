00902aec605084c1f7a27b77d67001d150272708
astralicc <orjyy.321@gmail.com>
2026-08-24 17:24:14 +0700

    fix: restore Tailwind CSS base directives and enable integration base styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    tailwind(),
     mdx(),
   ],
 });
